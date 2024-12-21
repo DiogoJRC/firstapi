f85315f04d2dd2cd74e674db75d3421f3b2dccac
Diogo Capdeville <diogoj.r.c@hotmail.com>
2024-12-21 15:30:09 -0300

    Recebendo parâmetros na URL

--- src/index.js
+++ src/index.js
@@ -10,14 +10,27 @@ const server = http.createServer((request, response) => {
     `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
   );
 
+  //a mesma coisa que let pathname = parsedUrl.pathname
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  //filter(Boolean) retira as strings vazias que retornaram do split
+  let splitEndpoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`;
+    id = splitEndpoint[1];
+  }
+
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === parsedUrl.pathname &&
-      routeObj.method === request.method
+      routeObj.endpoint === pathname && routeObj.method === request.method
   );
 
   if (route) {
     request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
     route.handler(request, response);
   } else {
     response.writeHead(404, { "content-type": "application/json" });
